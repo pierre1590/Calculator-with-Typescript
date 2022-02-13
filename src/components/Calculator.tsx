e0f4c424c9abe73481f0b48db89434335e058e10
Piero Sabino <piero.sa@icloud.com>
2022-02-13 13:13:09 +0100

    Create a calculator with Typescript 🚀

---
+++ src/components/Calculator.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const Container = styled.div`
+    background: #323232;
+    font-size: 1.5rem;
+    font-weight: bold;
+    color: #fff;
+    text-align: center;
+`;
+
+const Calculator = () => {
+  return <Container>Calculator  </Container>
+    
+}
+
+export default Calculator
